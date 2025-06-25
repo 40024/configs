f14e94c8b5c9ed321469c01a394f7cd9b5045620
K <da2d9rwh@duck.com>
2025-06-24 23:20:40 -0700

    m

---
+++ Android/Android bash.rc.sh
@@ -0,0 +1,24 @@
+
+#  Aliases 
+# Important files
+alias d=
+alias t=
+alias ta=
+alias tr=
+alias m=
+alias a=
+alias q=
+alias g=
+# Core
+alias z='cd'
+alias h='history'
+# Git
+alias gc="git add .; git commit -m"
+alias gl="git pull"
+alias gp="git push"
+alias gpl="git log --pretty=format:'%h %ad %s'"
+alias gcp="git add .; git commit -m m; git push"
+# One-offs
+alias n='nvim'
+alias fzf="fzf --bind 'enter:execute(nvim {})'"
+alias avenv="source venv/bin/activate"
