ee3f6132d5c21ca907b908c3766eef83a89a7b5f
K <da2d9rwh@duck.com>
2025-06-25 21:54:35 -0700

    m

--- Android/Android bash.rc.sh
+++ Android/Android bash.rc.sh
@@ -1,14 +1,13 @@
-
 #  Aliases 
 # Important files
-alias d= 
-alias t= 
-alias ta=
-alias m= 
-alias tr=
-alias q= 
-alias g= 
-alias a= 
+# alias d=
+# alias t=
+# alias ta=
+# alias m=
+# alias tr=
+# alias q=
+# alias g=
+# alias a=
 # Core
 alias z='cd'
 alias l='ls'
@@ -16,12 +15,12 @@ alias c='clear'
 # History
 alias h='history'
 # Git
-alias gc="git add .; git commit -m"
-alias gl="git pull"
-alias gp="git push"
-alias gpl="git log --pretty=format:'%h %ad %s'"
-alias gcp="git add .; git commit -m m; git push"
+alias gc='cd "/data/data/com.termux/files/home/storage/shared/Documents/Sync-Docs"; git add .; git commit -m'
+alias gl='cd "/data/data/com.termux/files/home/storage/shared/Documents/Sync-Docs"; git pull'
+alias gp='cd "/data/data/com.termux/files/home/storage/shared/Documents/Sync-Docs"; git push'
+alias gpl='cd "/data/data/com.termux/files/home/storage/shared/Documents/Sync-Docs"; git log --pretty=format:"%h %ad %s"'
+alias gcp='cd "/data/data/com.termux/files/home/storage/shared/Documents/Sync-Docs"; git add .; git commit -m m; git push'
 # One-offs
 alias n='nvim'
-alias fzf="fzf --bind 'enter:execute(nvim {})'"
-alias avenv="source venv/bin/activate"
+alias fzf='fzf --bind "enter:execute(nvim {})"'
+alias avenv='source venv/bin/activate'
