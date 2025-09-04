77062ec3fdde33c4a6282e1b6ed9a76f7e6b8059
K <da2d9rwh@duck.com>
2025-09-03 17:43:44 -0700

    m

--- Mobile/init.lua
+++ Mobile/init.lua
@@ -83,10 +83,24 @@ require('lualine').setup {
   },
 }
 
--- Hide default status indicator in favor of lualine
-vim.opt.showmode = false
-
 vim.keymap.set('n', '>', '>>', { noremap = true })
 vim.keymap.set('v', '>', '>>', { noremap = true })
 vim.keymap.set('n', '<', '<<', { noremap = true })
 vim.keymap.set('v', '<', '<<', { noremap = true })
+
+-- Phone only
+vim.opt.wrap = false
+
+-- Phone only
+-- Map <leader>q to :q without recording and without changing registers
+vim.keymap.set('n', '<leader>q', function()
+  -- stop recording if a user started it
+  if vim.fn.reg_recording() ~= '' then
+    -- if in insert mode, leave it first, then stop recording
+    vim.cmd('stopinsert')
+    vim.cmd('normal! <Esc>')
+    vim.cmd('stoprecording') -- stop recording (avoids changing registers)
+  end
+  -- finally quit
+  vim.cmd('q')
+end, { noremap = true, silent = true })
