94ca2c932c046c87fd9b88b06784b83e55fa236f
K <da2d9rwh@duck.com>
2025-11-07 23:57:15 -0800

    m

---
+++ _CORE/nvim-lazy/options.lua
@@ -0,0 +1,8 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+--
+-- Disable default indentation settings
+vim.opt.tabstop = 8 -- Number of spaces that a <Tab> in the file counts for
+vim.opt.expandtab = false -- Use spaces instead of tabs
+vim.g.autoformat = false
